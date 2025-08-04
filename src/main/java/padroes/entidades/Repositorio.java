7cfe11fae778ab45ef8fd065911bf95032501ab8
mirelasc23 <mirelamsccomach@gmail.com>
2025-08-03 21:58:03 -0300

    adiciona novo Repositorio

--- src/main/java/padroes/entidades/Repositorio.java
+++ src/main/java/padroes/entidades/Repositorio.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package padroes.entidades;
 
-/**
- *
- * @author Info
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Repositorio {
+    public static final Repositorio INSTANCE = new Repositorio();
+    private List<Produto> produtos = new ArrayList<>();
+    private List<Servico> servicos = new ArrayList<>();
+
+    public Repositorio() {
+        iniciaProdutos();
+        iniciaServicos();
+    }
+
+    private void iniciaProdutos() {
+        Produto medicaento = new Produto("medimento", 10, 2);
+    }
+
+    private void iniciaServicos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
     
 }
